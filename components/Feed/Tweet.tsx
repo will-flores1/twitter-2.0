807f56f74cd5b1cb46b3fa12637b68a5939cc361
Will Flores <willflores1221@gmail.com>
2022-07-14 16:28:09 -0400

    Add comment api & stylezzzz

--- components/Feed/Tweet.tsx
+++ components/Feed/Tweet.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Tweet } from "../../typings";
+import React, { useEffect, useState } from "react";
+import { Comment, CommentBody, Tweet } from "../../typings";
 import TimeAgo from "react-timeago";
 import {
 	ChatAlt2Icon,
@@ -7,18 +7,63 @@ import {
 	SwitchHorizontalIcon,
 	UploadIcon,
 } from "@heroicons/react/outline";
+import { fetchComments } from "../../utils/fetchComments";
+import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 
 interface Props {
 	tweet: Tweet;
 }
 
 function Tweet({ tweet }: Props) {
+	const [comments, setComments] = useState<Comment[]>([]);
+	const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false);
+	const [input, setInput] = useState<string>("");
+	const { data: session } = useSession();
+
+	const refreshComments = async () => {
+		const comments: Comment[] = await fetchComments(tweet._id);
+		setComments(comments);
+	};
+
+	useEffect(() => {
+		refreshComments();
+	}, []);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const commentToast = toast.loading("Posting Comment...");
+
+		// Comment Login
+		const comment: CommentBody = {
+			comment: input,
+			tweetId: tweet._id,
+			username: session?.user?.name || "Unknown User",
+			profileImg: session?.user?.image || "https://links.papareact.com/gll",
+		};
+
+		const result = await fetch("/api/addComment", {
+			body: JSON.stringify(comment),
+			method: "POST",
+		});
+
+		console.log("WOOHOO we made it", result);
+		toast.success("Comment Posted!", {
+			id: commentToast,
+		});
+
+		setInput("");
+		setCommentBoxVisible(false);
+		refreshComments();
+	};
+
 	return (
 		<div className="flex flex-col space-x-3 border-y p-5 border-gray-100">
 			<div className="flex space-x-3">
 				<img
 					className="h-10 w-10 rounded-full object-cover"
-					src={tweet.profileImg}
+					src={tweet.profileImg || "https://links.papareact.com/gll"}
 					alt="profile pic"
 				/>
 
@@ -47,9 +92,12 @@ function Tweet({ tweet }: Props) {
 			</div>
 
 			<div className="mt-5 flex justify-between">
-				<div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+				<div
+					onClick={(e) => session && setCommentBoxVisible(!commentBoxVisible)}
+					className="flex cursor-pointer items-center space-x-3 text-gray-400"
+				>
 					<ChatAlt2Icon className="h-5 w-5" />
-					<p>5</p>
+					<p>{comments.length}</p>
 				</div>
 
 				<div className="flex cursor-pointer items-center space-x-3 text-gray-400">
@@ -64,6 +112,55 @@ function Tweet({ tweet }: Props) {
 					<UploadIcon className="h-5 w-5" />
 				</div>
 			</div>
+
+			{/* Comment Box Logic */}
+			{commentBoxVisible && (
+				<form className="mt-3 flex space-x-3" onSubmit={handleSubmit}>
+					<input
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+						className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
+						type="text"
+						placeholder="Write a comment..."
+					/>
+					<button
+						disabled={!input}
+						type="submit"
+						className="text-twitter disabled:text-gray-200"
+					>
+						Post
+					</button>
+				</form>
+			)}
+
+			{comments?.length > 0 && (
+				<div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-sroll border-t border-gray-100 p-5">
+					{comments.map((comment) => (
+						<div key={comment._id} className="relative flex space-x-2">
+							<hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+							<img
+								src={comment.profileImg}
+								className="mt-2 h-7 w-7 rounded-full object-cover"
+								alt=""
+							/>
+							<div>
+								<div className="flex items-center space-x-1">
+									<p className="mr-1 font-bold">{comment.username}</p>
+									<p className="hidden text-sm text-gray-500 lg:inline">
+										@{comment.username.replace(/\s+/g, "").toLowerCase()} ·
+									</p>
+
+									<TimeAgo
+										className="text-sm text-gray-500"
+										date={comment._createdAt}
+									/>
+								</div>
+								<p>{comment.comment}</p>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
