4f2073c4f6973290dec5b6c4b839f8f8b7655bf2
Will Flores <willflores1221@gmail.com>
2022-07-14 01:01:54 -0400

    Responsive design

---
+++ components/Feed/Feed.tsx
@@ -0,0 +1,34 @@
+import { RefreshIcon } from "@heroicons/react/outline";
+import React from "react";
+import { Tweet } from "../../typings";
+import Tweetbox from "./Tweetbox";
+import TweetComponent from "./Tweet";
+
+interface Props {
+	tweets: Tweet[];
+}
+
+function Feed({ tweets }: Props) {
+	return (
+		<div className="col-span-7 lg:col-span-5 border-x">
+			<div className="flex items-centers justify-between">
+				<h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
+				<RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+			</div>
+
+			{/* Tweetbox */}
+			<div>
+				<Tweetbox />
+			</div>
+
+			{/* Feed */}
+			<div>
+				{tweets.map((tweet) => (
+					<TweetComponent key={tweet._id} tweet={tweet} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default Feed;
