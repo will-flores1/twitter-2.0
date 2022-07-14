5ba0db571eeb45b043a066819d700bef44e155cb
Will Flores <willflores1221@gmail.com>
2022-07-14 16:28:25 -0400

    Add refresh function

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,24 +1,43 @@
 import { RefreshIcon } from "@heroicons/react/outline";
-import React from "react";
+import React, { useState } from "react";
 import { Tweet } from "../../typings";
 import Tweetbox from "./Tweetbox";
 import TweetComponent from "./Tweet";
+import { fetchTweets } from "../../utils/fetchTweets";
+import toast from "react-hot-toast";
 
 interface Props {
 	tweets: Tweet[];
 }
 
-function Feed({ tweets }: Props) {
+function Feed({ tweets: tweetsProp }: Props) {
+	const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+	console.log(tweets);
+
+	const handleRefresh = async () => {
+		const refreshToast = toast.loading("Refreshing...");
+
+		const tweets = await fetchTweets();
+		setTweets(tweets);
+
+		toast.success("Feed Updated!", {
+			id: refreshToast,
+		});
+	};
+
 	return (
-		<div className="col-span-7 lg:col-span-5 border-x">
+		<div className="col-span-7 lg:col-span-5 border-x max-h-screen overflow-scroll scrollbar-hide">
 			<div className="flex items-centers justify-between">
 				<h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-				<RefreshIcon className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+				<RefreshIcon
+					onClick={handleRefresh}
+					className="mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+				/>
 			</div>
 
 			{/* Tweetbox */}
 			<div>
-				<Tweetbox />
+				<Tweetbox setTweets={setTweets} />
 			</div>
 
 			{/* Feed */}
