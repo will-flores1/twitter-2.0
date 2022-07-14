b7e77c37894958efce2294c1f7d2dda1fc19eb88
Will Flores <willflores1221@gmail.com>
2022-07-14 16:30:09 -0400

    Add session

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -10,8 +10,11 @@ import {
 	HomeIcon,
 } from "@heroicons/react/outline";
 import SidebarRow from "./SidebarRow";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
+	const { data: session } = useSession();
+
 	return (
 		<div className="flex flex-col col-span-2 items-center px-4 md:items-start">
 			<img
@@ -25,7 +28,11 @@ function Sidebar() {
 			<SidebarRow Icon={MailIcon} title="Messages" />
 			<SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
 			<SidebarRow Icon={CollectionIcon} title="Lists" />
-			<SidebarRow Icon={UserIcon} title="Sign In" />
+			<SidebarRow
+				onClick={session ? signOut : signIn}
+				Icon={UserIcon}
+				title={session ? "Sign Out" : "Sign In"}
+			/>
 			<SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
 		</div>
 	);
