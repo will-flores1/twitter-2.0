d846065155cf830afc05a64170d1652a7af4c5f6
Will Flores <willflores1221@gmail.com>
2022-07-15 17:58:25 -0400

    FIx Vercel deploy issue

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,7 +1,7 @@
 import { Comment } from "../typings";
 
 export const fetchComments = async (tweetId: string) => {
-  const res = await fetch(`/api/getComments?tweetId=${tweetId}`);
+  const res = await fetch(`https://twitter-2-0-nu.vercel.app/api/getComments?tweetId=${tweetId}`);
 
   const comments: Comment[] = await res.json();
 
